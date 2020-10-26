47ae02f00e50282ac054ad20effbf212b16673f7
n-pretorious <pretoriousndungu@gmail.com>
2020-10-26 22:10:06 +0300

    add user profile model and view

--- vote/models.py
+++ vote/models.py
@@ -1,20 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Profile(models.Model):
-  photo = CloudinaryField('photo')
-  name=models.CharField(max_length=20)
-  userName=models.CharField(max_length=10, blank=True)
-  contact=models.EmailField(max_length=254)
-  bio=models.TextField()
-  
-  def __str__(self):
-    return self.name
-  
-  class Meta:
-      ordering = ['name']
-      
-  def saveProfile(self):
-    self.save()
-  
+    user = models.OneToOneField(User, null=True, on_delete=CASCADE, related_name='userProfile')
+    photo = CloudinaryField("photo")
+    name = models.CharField(max_length=20)
+    userName = models.CharField(max_length=10, blank=True)
+    contact = models.EmailField(max_length=254)
+    country = models.CharField(max_length=20, null=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+    class Meta:
+        ordering = ["name"]
+
+    def saveProfile(self):
+        self.save()
+
+    @classmethod
+    def getProfileById(cls, pk):
+        try:
+            profileObject = cls.objects.get(id=pk)
+            return profileObject
+        except ObjectDoesNotExist:
+            message = "Profile does not exist"
+            return message
--- vote/views.py
+++ vote/views.py
@@ -1,5 +1,30 @@
+from django.http.response import Http404, HttpResponse, HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist
+from awards.settings import LOGOUT_REDIRECT_URL
+from vote.models import Profile
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def profile(request):
+    try:
+        profile = Profile.objects.get(user = request.user)
+        print(profile)
+    except ObjectDoesNotExist:
+        raise Http404()
+
+    context = {
+      'profile': profile
+    }
+    return render(request, "profile.html", context)
+
+    # else:
+    #   return HttpResponse('not found')
+
+
+def logout_view(request):
+    logout(request)

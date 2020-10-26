53e56a30785d5907626468933ce1d3e4432baccc
n-pretorious <pretoriousndungu@gmail.com>
2020-10-26 14:16:19 +0300

    add profile model test

--- vote/tests.py
+++ vote/tests.py
@@ -1,3 +1,22 @@
+from vote.models import Profile
 from django.test import TestCase
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+  def setUp(self):
+    self.profile = Profile(
+      photo = 'test.jpeg',
+      name = 'Test Name',
+      userName = 'testUser',
+      contact = 'test@user.com',
+      bio = 'This is a bio of a test user who is running texts to make sure the models are working properly.'
+    )
+    self.profile.save()
+    
+  def testInstance(self):
+    self.assertTrue(self.profile, Profile)
+    
+  def testSave(self):
+    self.profile.saveProfile()
+    profile = Profile.objects.all()
+    self.assertTrue(len(profile) > 0)

61791d352e2f9353eeb8c6de05e04c06a5fc1e8c
n-pretorious <pretoriousndungu@gmail.com>
2020-10-26 14:16:09 +0300

    add profile model

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+  photo = CloudinaryField('photo')
+  name=models.CharField(max_length=20)
+  userName=models.CharField(max_length=10, blank=True)
+  contact=models.EmailField(max_length=254)
+  bio=models.TextField()
+  
+  def __str__(self):
+    return self.name
+  
+  class Meta:
+      ordering = ['name']
+      
+  def saveProfile(self):
+    self.save()
+  

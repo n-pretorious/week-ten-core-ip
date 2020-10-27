1b397d22a1be011c8fecdf9e7457236776a5298a
n-pretorious <pretoriousndungu@gmail.com>
2020-10-27 19:40:42 +0300

    add project model

--- vote/admin.py
+++ vote/admin.py
@@ -1,5 +1,6 @@
-from vote.models import Profile
+from vote.models import Profile, Project
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Project)

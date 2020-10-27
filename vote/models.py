2411d792e8c04d59443a8e7c3704c523c9ab000f
n-pretorious <pretoriousndungu@gmail.com>
2020-10-27 20:44:06 +0300

    add rating model

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -6,8 +7,10 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=CASCADE, related_name='userProfile')
-    photo = CloudinaryField('photo')
+    user = models.OneToOneField(
+        User, null=True, on_delete=CASCADE, related_name="userProfile"
+    )
+    photo = CloudinaryField("photo")
     name = models.CharField(max_length=20)
     userName = models.CharField(max_length=10, blank=True)
     contact = models.EmailField(max_length=254)
@@ -18,7 +21,7 @@ class Profile(models.Model):
         return str(self.user)
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
     def saveProfile(self):
         self.save()
@@ -29,18 +32,42 @@ class Profile(models.Model):
             profileObject = cls.objects.get(id=pk)
             return profileObject
         except ObjectDoesNotExist:
-            message = 'Profile does not exist'
+            message = "Profile does not exist"
             return message
-          
+
+
 class Project(models.Model):
-    image = CloudinaryField('image')
+    image = CloudinaryField("image")
     title = models.CharField(max_length=30)
     description = models.TextField()
     link = models.CharField(max_length=255)
     profile = models.ForeignKey(User, null=True, on_delete=CASCADE)
-    
+
     def __str__(self):
-      return self.title
-    
+        return self.title
+
     def saveProject(self):
+        self.save()
+
+    @classmethod
+    def getProjectById(cls, pk):
+        try:
+            projectObject = cls.objects.get(id=pk)
+            return projectObject
+        except ObjectDoesNotExist:
+            message = "Project does not exist"
+            return message
+
+
+class Rate(models.Model):
+    project = models.ForeignKey(Project, null=True, on_delete=CASCADE)
+    design = models.IntegerField(null=True, blank=True)
+    usability = models.IntegerField(null=True, blank=True)
+    content = models.IntegerField(null=True, blank=True)
+    author = models.ForeignKey(User, null=True, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.project
+
+    def saveRating(self):
       self.save()

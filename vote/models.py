4fe474a34439719823a50eeb29622234db4bf80c
n-pretorious <pretoriousndungu@gmail.com>
2020-10-27 19:39:57 +0300

    add project model

--- vote/models.py
+++ vote/models.py
@@ -7,7 +7,7 @@ from django.db.models.deletion import CASCADE
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=CASCADE, related_name='userProfile')
-    photo = CloudinaryField("photo")
+    photo = CloudinaryField('photo')
     name = models.CharField(max_length=20)
     userName = models.CharField(max_length=10, blank=True)
     contact = models.EmailField(max_length=254)
@@ -18,7 +18,7 @@ class Profile(models.Model):
         return str(self.user)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ['name']
 
     def saveProfile(self):
         self.save()
@@ -29,5 +29,18 @@ class Profile(models.Model):
             profileObject = cls.objects.get(id=pk)
             return profileObject
         except ObjectDoesNotExist:
-            message = "Profile does not exist"
+            message = 'Profile does not exist'
             return message
+          
+class Project(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    link = models.CharField(max_length=255)
+    profile = models.ForeignKey(User, null=True, on_delete=CASCADE)
+    
+    def __str__(self):
+      return self.title
+    
+    def saveProject(self):
+      self.save()
